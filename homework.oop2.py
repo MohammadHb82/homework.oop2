86a8b7a7fca27eeb8182e2a0d28ba0ae74756616
Mohammad harb <136888573+MohammadHb82@users.noreply.github.com>
2023-08-04 16:32:45 -0700

    Create homework.oop2.py

---
+++ homework.oop2.py
@@ -0,0 +1,34 @@
+#example1:
+# class person:
+#     name="mohammad harb"
+#     age=20
+#     yniversity="YU"
+#     def nameOffactuly(self):
+#         print(self.yniversity)
+#     def nameOfme(self):
+#         print(self.name)
+#     def ageForme(self):
+#         print(self.age)
+# answer=person()
+# print(answer.nameOffactuly())
+# print(answer.ageForme())
+# print(answer.nameOfme())
+#example2:
+# class car:
+#     model="2022"
+#     name="ferrari"
+#     engine="800cc"
+#     def name_Of_ThisCar(self):
+#         print("This car was manufactured in",self.model)
+#     def name_of_car(self):
+#         for i in range(len(self.name)):
+            
+#             New_name=self.name[0:4] + "<3" + self.name[4:6]
+#             return New_name
+#     def power_of_engine(self):
+#         print(self.engine)
+# car1=car()
+# print(car1.name_of_car())
+# print(car1.power_of_engine())
+
+     
